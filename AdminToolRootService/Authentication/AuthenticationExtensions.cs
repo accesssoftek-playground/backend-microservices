c5647860e589fda59a50b74f58987a599b9da9a8
Serge Loboda <serge.loboda@gmail.com>
2022-11-11 21:45:30 +0200

    The first commit of the service with JWT authentication

---
+++ AdminToolRootService/Authentication/AuthenticationExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AdminToolRootService.Authentication;
+
+internal static class AuthenticationExtensions
+{
+    public static IServiceCollection AddAuthenticationWithJwt(this IServiceCollection services, bool isDevelopment, string publicKeyJwt)
+        {
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = !isDevelopment;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        // TODO: Move to config
+                        ValidIssuer = "http://localhost:6080/realms/AdminToolPrototype",
+                        ValidAudience = "http://localhost:5000",
+                        IssuerSigningKey = BuildRsaKey(publicKeyJwt)
+                    };
+                    if (isDevelopment)
+                    {
+                        options.Events = new JwtBearerEvents
+                        {
+                            OnAuthenticationFailed = context =>
+                            {
+                                Console.WriteLine("Authentication failed: " + context.Exception.Message);
+                                return Task.CompletedTask;
+                            },
+                            OnTokenValidated = context =>
+                            {
+                                Console.WriteLine("Token validated: " + context.SecurityToken);
+                                return Task.CompletedTask;
+                            }
+                        };
+                    }
+                });
+            
+            return services;
+        }
+    private static RsaSecurityKey BuildRsaKey(string publicKeyJwt)
+    {
+        var rsa = RSA.Create();
+        rsa.ImportSubjectPublicKeyInfo(source: Convert.FromBase64String(publicKeyJwt), bytesRead: out _);
+        return new RsaSecurityKey(rsa);
+    }
+}
---
+++ AdminToolRootService/Startup.cs
@@ -0,0 +1,42 @@
+﻿using AdminToolRootService.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace AdminToolRootService;
+
+internal sealed class Startup
+{
+    private readonly IConfiguration _configuration;
+    private readonly IWebHostEnvironment _environment;
+    
+    public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        _configuration = configuration;
+        _environment = environment;
+    }
+
+    // TODO: Move to config
+    private const string PublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4uWOVsJGjWQK7aHzjK0OspJwv+/dX4qVXlyPhU0ngi3n1Tpm8fVvJgE6egqb28AYxOB4d3/vE2zeqG/O6JKnoWdliK/IhcX4LrZOxyPPf8eizvahgfqyAyfLrlHAJxnNdjjJsog10pSVjtAtbwH1bF+JtMubdpVSMs1HKEE4Cpbr0ks2gis0Dpq/R33uF9woAgoLfpVk+9cPsde3sJ0k7W9xufmkP4yGZfzuAQs+XEEJtBpgvETBYkKXYidiDuLxQIq0ilYZ0yusY3RbCGKxqhlxzVh0bRswFgGUXZjc/yQRGS1ZUf+Q3WIszfCOFuQqeMUeBYgiou8O1+Z8BJSkPwIDAQAB";
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAuthenticationWithJwt(_environment.IsDevelopment(), PublicKey);
+        services.AddControllers();
+    }
+    
+    public void Configure(IApplicationBuilder app)
+    {
+        if (_environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });        
+    }
+}
